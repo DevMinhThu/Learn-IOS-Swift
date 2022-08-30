efdb6fa189f6f3cd698fed5218bf97861f6f45b9
Vu Minh Thu <devminhthu2023@gmail.com>
2022-08-30 16:33:00 +0700

    Section 7: Using and Understanding Apple Documentation and using function in Swift

---
+++ Section07-Using-and-Understanding-Apple-Documentation/Xylophone-iOS13/Xylophone/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by MinhThu Vu on 30/08/2022.
+//  Copyright © 2022 The App Xylophone.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(soundName: sender.currentTitle!)
+    }
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player?.play()
+    }
+
+}
+
