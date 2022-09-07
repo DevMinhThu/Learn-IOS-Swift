e220fe87f29159bc0745b89a7b759a96d27a4536
Vu Minh Thu <devminhthu2023@gmail.com>
2022-09-07 08:25:46 +0700

    feat: Optimizing the code according to the MVC model

---
+++ Section09-iOS-App-Design-Patterns-and-Code-Structuring/Quizzler-iOS13/Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by MinhThu Vu on 05/09/2022.
+//  Copyright © 2022 The App Quizzler.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var labelQuestion: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    var quizBrain = QuizBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        // viewDidLoad() chỉ được kích hoạt 1 lần đầu tiên màn hình hiển thị
+        updateUI()
+    }
+    
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle!
+        let userGotItRight = quizBrain.checkAnswer(userAnswer)
+        
+        if userGotItRight {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+        
+        // Handle next question
+        quizBrain.nextQuestion()
+        
+        // sau khoảng 2s mới update lại UI
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+    }
+    
+    @objc func updateUI() {
+        // progress bar
+        progressBar.progress = quizBrain.getProgress()
+        labelQuestion.text = quizBrain.getQuestionText()
+        
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+    }
+}
+
